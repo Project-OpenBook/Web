c07bf1236e06144ed0d3064ca9eed828d4ac9dcd
조민기 <58856846+Nail0606@users.noreply.github.com>
2024-06-21 03:06:31 +0900

    Refactor : 부스 설명 입력 Input 생성

---
+++ src/Components/Booth/Regist/Input/LocationInput.tsx
@@ -0,0 +1,25 @@
+export default function LocationInput() {
+  return (
+    <div className="flex flex-col w-1/2 mb-5">
+      <div className="flex gap-2 items-center h-full mb-2">
+        <SlLocationPin size={25} color="#0064FF" />
+        <label className="font-bold">부스 위치</label>
+      </div>
+      <div className="flex items-center w-full gap-2">
+        <input
+          placeholder="원하는 부스 신청 위치를 선택해주세요"
+          type="text"
+          className="h-10 border-b-2 pl-1 w-3/4"
+          onChange={(e) => {}}
+          value={selectedSeatNumbers.join(", ")}
+        />
+        <button
+          className="h-8 w-1/4 hover:cursor-pointer bg-[#0064FF] rounded-md text-white"
+          onClick={switchModal}
+        >
+          선택
+        </button>
+      </div>
+    </div>
+  );
+}
