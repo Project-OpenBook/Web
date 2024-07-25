6f8712fc6ce8811d75a2414719e9f8c7d8e7d9e3
Goongam <dlwjddn341@naver.com>
2024-07-25 19:36:30 +0900

    feat: tailwind 설정 위치를 extend로 변경

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,19 +8,21 @@ export default {
   purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
   darkMode: false, // or 'media' or 'class',
   theme: {
-    extend: {},
+    extend: {
+      backgroundColor: {
+        mainBlue: '#0064FF',
+        mainRed: '#EF4444'
+      },
+        borderColor: {
+          mainGray: '#D1D5DB'
+      }
+    },
     fontFamily: {
       nanumEB: ["nanumEB"],
       nanumB: ["nanumB"],
       nanum: ["namnum"],
     },
-    backgroundColor:{
-      mainBlue: '#0064FF',
-      mainRed: '#EF4444'
-    },
-    // borderColor:{ // 기본 보더가 바뀌어서 주석처리
-    //   mainGray: '#D1D5DB'
-    // }
+
   },
   plugins: [],
 };
