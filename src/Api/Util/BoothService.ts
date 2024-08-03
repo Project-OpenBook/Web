abdc99f021cd5721c28395ae68637c8e81238dfe
Sunho-97 <cow528@naver.com>
2024-08-03 22:26:29 +0900

    Feat : 최신순, 오래된순 필터 기능 추가

--- src/Api/Util/BoothService.ts
+++ src/Api/Util/BoothService.ts
@@ -17,11 +17,14 @@ export interface BoothResponse {
 }
 
 export const fetchBooths = async (
-  sliceNumber: number
+  sliceNumber: number,
+  sortOrder: string
 ): Promise<BoothResponse> => {
   const token = getAccessToken();
   const response = await fetch(
-    `http://52.79.91.214:8080/booths?page=${sliceNumber}`,
+    `http://52.79.91.214:8080/booths?page=${sliceNumber}&sort=openTime%2C${
+      sortOrder === "최신순" ? "DESC" : "ASC"
+    }&progress=ongoing`,
     {
       method: "GET",
       headers: {
