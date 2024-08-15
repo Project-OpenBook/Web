ad5e0d435dd817c3a901d0ea2be4d5a3374d20c5
Goongam <dlwjddn341@naver.com>
2024-08-15 22:00:46 +0900

    feat: 내 부스 데이터 무한스크롤로 연결

--- src/Components/MyPage/MyBooth.tsx
+++ src/Components/MyPage/MyBooth.tsx
@@ -1,55 +1,103 @@
+import { useInfiniteQuery } from "@tanstack/react-query";
+import { getAccessToken } from "../../Api/Util/token";
+import { useEffect, useState } from "react";
+import RadioButtons from "../Event/List/RadioButtons";
+import { OrderType } from "../../Api/Util/EventService";
+import InfiniteScroll from "react-infinite-scroll-component";
 import BoothCard from "../Booth/List/BoothCard";
-import EventCard from "../Event/List/EventCard";
+import { Booth } from "../../Api/Util/BoothService";
+import { useScrollDown } from "../../Hooks/useScrollDown";
+
+enum BoothStatus {
+  all = "ALL",
+  aprove = "APPROVE",
+  waiting = "WAITING",
+  reject = "REJECT",
+}
+
+enum BoothSort {
+  desc = "DESC",
+  asc = "ASC",
+}
+
+interface InfinityMyEvent {
+  hasNext: boolean;
+  numberOfElements: number;
+  sliceNumber: number;
+  content: Array<Booth>;
+}
+
+const fetcher = (
+  boothSort: OrderType,
+  boothStatus: BoothStatus,
+  page: number
+) =>
+  fetch(
+    `http://52.79.91.214:8080/manage/booths?status=${boothStatus}&sort=registeredAt%2C${
+      boothSort === "최신순" ? BoothSort.desc : BoothSort.asc
+    }&page=${page}`,
+    {
+      method: "GET",
+      headers: {
+        Authorization: `Bearer ${getAccessToken()}`,
+      },
+    }
+  ).then((response) => {
+    if (response.ok) return response.json();
+    else throw new Error();
+  });
 
 export default function MyBooth() {
-  //   const tmp
-  const tmpBoothList = [
-    {
-      endDate: "2024-00-00",
-      id: 25,
-      image:
-        "https://openbookbucket.s3.ap-northeast-2.amazonaws.com/%E1%84%82%E1%85%A9%E1%84%85%E1%85%A2%E1%84%87%E1%85%A1%E1%86%BC%E1%84%87%E1%85%AE%E1%84%89%E1%85%B3.png",
-      name: "부스명",
-    },
-    {
-      endDate: "2024-00-00",
-      id: 1,
-      image: "https://openbookbucket.s3.ap-northeast-2.amazonaws.com/booo.png",
-      name: "부스명",
-    },
-    {
-      endDate: "2024-00-00",
-      id: 1,
-      image:
-        "https://openbookbucket.s3.ap-northeast-2.amazonaws.com/%E1%84%90%E1%85%A2%E1%84%80%E1%85%AE%E1%86%A8%E1%84%87%E1%85%AE%E1%84%89%E1%85%B3.png",
-      name: "부스명",
-    },
-    {
-      endDate: "2024-00-00",
-      id: 1,
-      image:
-        "https://openbookbucket.s3.ap-northeast-2.amazonaws.com/f49d8fd1-7de5-4e9a-8c7e-4c4d7eca10d0%E1%84%8E%E1%85%B5%E1%84%86%E1%85%A2%E1%86%A8%E1%84%87%E1%85%AE%E1%84%89%E1%85%B3.png",
-      name: "부스명",
-    },
-    {
-      endDate: "2024-00-00",
-      id: 1,
-      image:
-        "https://openbookbucket.s3.ap-northeast-2.amazonaws.com/86eb0a5f-41e6-42e8-bd17-49d8e8460d4bmakaron.png",
-      name: "부스명",
-    },
-  ];
+  const [boothStatus] = useState<BoothStatus>(BoothStatus.all);
+  const [boothSort, setBoothSort] = useState<OrderType>("최신순");
+
+  const { data, fetchNextPage, hasNextPage, isError, refetch } =
+    useInfiniteQuery({
+      queryKey: ["mybooth"],
+      queryFn: ({ pageParam }) => fetcher(boothSort, boothStatus, pageParam),
+      getNextPageParam: (lastPage: InfinityMyEvent) =>
+        (lastPage.hasNext && lastPage.sliceNumber + 1) || undefined,
+      initialPageParam: 0,
+    });
+
+  useEffect(() => {
+    refetch();
+  }, [boothSort, refetch]);
+
+  useScrollDown({
+    onScrollDownToEnd: fetchNextPage,
+  });
+
+  if (isError && !data?.pages) {
+    return <>내 부스 데이터를 가져오는데 실패했습니다.</>;
+  }
 
   return (
-    <section className="flex flex-col gap-4">
-      {/* {tmpBoothList.map((booth) => (
-        <BoothCard
-          endDate={booth.endDate}
-          id={booth.id}
-          image={booth.image}
-          name={booth.name}
-        />
-      ))} */}
-    </section>
+    <InfiniteScroll
+      dataLength={data?.pages[0].numberOfElements ?? 6}
+      next={fetchNextPage}
+      hasMore={hasNextPage}
+      loader={<h4 className="text-center my-4">로딩 중...</h4>}
+      endMessage={
+        <p className="text-center font-bold my-4">모든 행사를 불러왔습니다</p>
+      }
+    >
+      <section className="flex flex-col gap-4">
+        <RadioButtons sortOrder={boothSort} onSortOrderChange={setBoothSort} />
+        {data?.pages.map((myBooth) =>
+          myBooth.content.map((booth) => (
+            <BoothCard
+              endDate={booth.closeDate}
+              id={booth.id}
+              image={booth.mainImageUrl}
+              name={booth.name}
+              key={booth.id}
+              eventName={booth.eventName}
+              // tags={booth.tags} TODO: 부스 tags 브랜치 머지 후 주석 해제
+            />
+          ))
+        )}
+      </section>
+    </InfiniteScroll>
   );
 }
