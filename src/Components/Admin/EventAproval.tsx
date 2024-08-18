9c107f109fdcb05ecdee5af123403889efecfbe4
Goongam <dlwjddn341@naver.com>
2024-08-18 19:49:16 +0900

    feat: 불필요 주석 제거 & 관리자 페이지 스크롤

--- src/Components/Admin/EventAproval.tsx
+++ src/Components/Admin/EventAproval.tsx
@@ -1,9 +1,9 @@
 import { useQuery } from "@tanstack/react-query";
-import { ChangeEvent, useEffect, useState } from "react";
+import { useEffect } from "react";
 import { useRadioChecks } from "../../Hooks/useRadioChecks";
 import { getAccessToken } from "../../Api/Util/token";
 import PageNation from "../Util/PageNation";
-import { Link, useSearchParams } from "react-router-dom";
+import { useSearchParams } from "react-router-dom";
 import PleaseLogin from "../Login/PleaseLogin";
 import { useAproval } from "../../Hooks/useAproval";
 
@@ -13,7 +13,7 @@ interface EventAprovalType {
     description: string;
     location: string;
     name: string;
-    registrationDate: string;
+    openDate: string;
     status: string;
   }>;
   pageNumber: number;
@@ -84,20 +84,8 @@ export default function EventAproval() {
 
   if (isError) return <>행사 요청 데이터를 가져오는데 실패했습니다.</>;
   return (
-    <div className="flex-1 flex flex-col p-2">
+    <div className="flex-1 w-full flex flex-col p-2">
       <div className="w-full inline-flex gap-3 p-2">
-        {/* <img
-          className="border p-2 rounded-md"
-          src=""
-          alt="설정"
-          onClick={() => console.log(checkList)}
-        ></img> */}
-        {/* <button
-          className="border p-2 rounded-md"
-          onClick={() => changeStates("WATING")}
-        >
-          승인 대기
-        </button> */}
         <button
           className="border p-2 px-4 rounded-md font-bold text-white bg-green-400"
           onClick={() => changeStates("APPROVE")}
@@ -110,72 +98,79 @@ export default function EventAproval() {
         >
           반려
         </button>
-        {/* <button className="border p-2 rounded-md ml-auto">선택 삭제</button> */}
       </div>
-      <div className="container mx-auto">
-        <table className="min-w-full bg-white border-y border-gray-200">
-          <thead>
-            <tr className="border-b">
-              <th className="py-2 w-1">
-                <input
-                  type="checkbox"
-                  checked={isCheckAll}
-                  onChange={clickCheckAll}
-                />
-              </th>
-              <th className="py-2 px-4">행사명</th>
-              <th className="py-2 px-4">행사 장소</th>
-              <th className="py-2 px-4">행사 신청일</th>
-              <th className="py-2 px-4">행사 설명</th>
-              <th className="py-2 px-4">상태</th>
-              <th className="py-2 px-4">관리</th>
-            </tr>
-          </thead>
-          <tbody>
-            {data?.content?.map((booth, index) => (
-              <tr key={index} className="text-center">
-                <td className="py-2 px-4 border-b">
+      <div className="overflow-x-auto">
+        <div className="container mx-auto">
+          <table className="w-[800px] bg-white border-y border-gray-200">
+            <thead>
+              <tr className="border-b">
+                <th className="py-2 w-1">
                   <input
                     type="checkbox"
-                    onChange={(e) => clickCheckbox(e, index)}
-                    checked={checkList[index] ?? false}
+                    checked={isCheckAll}
+                    onChange={clickCheckAll}
                   />
-                </td>
-                <td className="py-2 px-4 border-b">{booth.name}</td>
-                <td className="py-2 px-4 border-b">{booth.location}</td>
-                <td className="py-2 px-4 border-b">{booth.registrationDate}</td>
-                <td className="py-2 px-4 border-b">{booth.description}</td>
-                <td
-                  className={`py-2 px-4 border-b ${
-                    booth.status === "REJECT"
-                      ? "text-red-500"
-                      : booth.status === "APPROVE"
-                      ? "text-green-500"
-                      : ""
-                  }`}
-                >
-                  {booth.status}
-                </td>
-                <td className="py-2 px-4 border-b">
-                  <button
-                    className="w-full text-white bg-green-400 shadow-md hover:underline mr-2 border rounded-md px-2 whitespace-nowrap"
-                    onClick={() => onAprove(booth.id)}
-                  >
-                    승인
-                  </button>
-                  <button
-                    className="w-full text-white bg-red-400 shadow-md hover:underline border rounded-md px-2 whitespace-nowrap"
-                    onClick={() => onReject(booth.id)}
-                  >
-                    반려
-                  </button>
-                </td>
+                </th>
+                <th className="py-2 px-4">행사명</th>
+                <th className="py-2 px-4">행사 장소</th>
+                <th className="py-2 px-4">행사 신청일</th>
+                <th className="py-2 px-4">행사 설명</th>
+                <th className="py-2 px-4">상태</th>
+                <th className="py-2 px-4">관리</th>
               </tr>
-            ))}
-          </tbody>
-        </table>
-        {data && <PageNation maxPage={data.totalPages ?? 1} showPage={5} />}
+            </thead>
+            <tbody>
+              {data?.content?.map((booth, index) => (
+                <tr key={index} className="text-center">
+                  <td className="py-2 px-4 border-b">
+                    <input
+                      type="checkbox"
+                      onChange={(e) => clickCheckbox(e, index)}
+                      checked={checkList[index] ?? false}
+                    />
+                  </td>
+                  <td className="py-2 px-4 border-b">{booth.name}</td>
+                  <td className="py-2 px-4 border-b">{booth.location}</td>
+                  <td className="py-2 px-4 border-b">{booth.openDate}</td>
+                  <td className="py-2 px-4 border-b">{booth.description}</td>
+                  <td
+                    className={`py-2 px-4 border-b ${
+                      booth.status === "REJECT"
+                        ? "ext-red-500"
+                        : booth.status === "APPROVE"
+                        ? "text-green-500"
+                        : ""
+                    }`}
+                  >
+                    {booth.status}
+                  </td>
+                  <td className="py-2 px-4 border-b">
+                    <button
+                      className="w-full text-white bg-green-400 shadow-md hover:underline mr-2 border rounded-md px-2 whitespace-nowrap"
+                      onClick={() => onAprove(booth.id)}
+                    >
+                      승인
+                    </button>
+                    <button
+                      className="w-full text-white bg-red-400 shadow-md hover:underline border rounded-md px-2 whitespace-nowrap"
+                      onClick={() => onReject(booth.id)}
+                    >
+                      반려
+                    </button>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
       </div>
+      {data && (
+        <PageNation
+          maxPage={data.totalPages ?? 1}
+          showPage={5}
+          className="mt-5"
+        />
+      )}
     </div>
   );
 }
--- src/Components/Layout/RequestLayout.tsx
+++ src/Components/Layout/RequestLayout.tsx
@@ -9,11 +9,12 @@ export default function RequestLayout({ children, header, side }: Props) {
       <div className="w-full max-w-screen-lg border h-full p-10">
         <div className="flex items-center gap-20 border-b p-7">
           <h2 className="font-extrabold text-4xl">{header}</h2>
-          {/* <span className="font-bold text-2xl">{"행사명"}</span> */}
         </div>
         <div className="flex">
           {side && (
-            <div className="w-32 h-80 border-r p-4 font-bold">{side}</div>
+            <div className="w-32 shrink-1 border-r p-4 font-bold hidden lg:flex">
+              {side}
+            </div>
           )}
           {children}
         </div>
