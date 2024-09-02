3e7633a79ec61f6037c1de2b7432c2002389a0f4
Sunho-97 <cow528@naver.com>
2024-09-02 14:17:19 +0900

    UI : 알림 모달 UI 및 위치 수정

--- src/Components/Alarm/AlarmModal.tsx
+++ src/Components/Alarm/AlarmModal.tsx
@@ -16,11 +16,9 @@ export default function AlarmModal({
       shouldCloseOnOverlayClick={true}
       isOpen={isOpen}
       onRequestClose={onRequestClose}
-      className="absolute top-32 right-11 flex flex-col w-1/4 h-5/6 bg-white p-4 rounded border border-gray-300 outline-none -translate-x-3.5 -translate-y-4"
+      className="absolute top-28 right-6 flex flex-col w-1/4 h-5/6 bg-white p-4 rounded border border-gray-300 outline-none -translate-x-3.5 -translate-y-4"
       overlayClassName="inset-0"
     >
-      <div className="absolute top-0 right-2/3 transform translate-x-16 -translate-y-full h-0 w-0 border-x-8 border-x-transparent border-b-8 border-b-gray-300"></div>
-      <div className="absolute top-px right-2/3 transform translate-x-16 -translate-y-full h-0 w-0 border-x-8 border-x-transparent border-b-8 border-b-white"></div>
       <div className="flex-1 overflow-y-auto">{children}</div>
     </ReactModal>
   );
