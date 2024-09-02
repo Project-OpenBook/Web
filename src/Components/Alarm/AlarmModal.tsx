ffdd20bd946642021270d823853bbf87c123acf9
Sunho-97 <cow528@naver.com>
2024-09-02 14:57:26 +0900

    UI : 알림 모달창 길이 수정

--- src/Components/Alarm/AlarmModal.tsx
+++ src/Components/Alarm/AlarmModal.tsx
@@ -16,7 +16,7 @@ export default function AlarmModal({
       shouldCloseOnOverlayClick={true}
       isOpen={isOpen}
       onRequestClose={onRequestClose}
-      className="absolute top-28 right-6 flex flex-col w-1/4 h-5/6 bg-white p-4 rounded border border-gray-300 outline-none -translate-x-3.5 -translate-y-4"
+      className="absolute top-28 right-6 flex flex-col w-1/4 h-2/3 bg-white p-4 rounded border border-gray-300 outline-none -translate-x-3.5 -translate-y-4"
       overlayClassName="inset-0"
     >
       <div className="flex-1 overflow-y-auto">{children}</div>
