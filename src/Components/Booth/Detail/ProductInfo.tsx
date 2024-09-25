c40321f4634a822fcaece003051a44e660616405
조민기 <58856846+Nail0606@users.noreply.github.com>
2024-09-26 02:53:24 +0900

    Chore : 부스 더미데이터 변경

--- src/Components/Booth/Detail/ProductInfo.tsx
+++ src/Components/Booth/Detail/ProductInfo.tsx
@@ -7,10 +7,10 @@ export default function ProductInfo() {
         alt="부스 이미지"
       />
       <div className="flex flex-col w-5/6 gap-3">
-        <div>물품명 : 농담곰 인형</div>
+        <div>물품명 : 간장 닭꼬치</div>
         <div>재고 : 100 개</div>
-        <div>설명 : 농담곰 인형입니다 귀엽죠</div>
-        <div>가격 : 8000 원</div>
+        <div>설명 : 매운 닭꼬치입니다.</div>
+        <div>가격 : 3000 원</div>
       </div>
     </div>
   );
