cb51820bf6d80b7797fa025f73b1091f26a8e692
Goongam <dlwjddn341@naver.com>
2024-10-06 19:31:18 +0900

    feat: 북마크

---
+++ src/Components/Bookmark/BookmarkIcon.tsx
@@ -0,0 +1,59 @@
+import { FaBookmark, FaRegBookmark } from "react-icons/fa6";
+import { getAccessToken } from "../../Api/Util/token";
+
+interface Props {
+  isBookmark: boolean;
+  type: "EVENT" | "BOOTH";
+  id: number;
+  className?: string;
+}
+
+export default function BookmarkIcon({
+  isBookmark,
+  type,
+  id,
+  className,
+}: Props) {
+  const onAddBookmark = () => {
+    fetch(`http://52.79.91.214:8080/bookmark`, {
+      method: "POST",
+      body: JSON.stringify({
+        type,
+        resourceId: id,
+        alarmSet: false,
+      }),
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${getAccessToken()}`,
+      },
+    });
+  };
+
+  const onDeleteBookmark = () => {
+    fetch(`http://52.79.91.214:8080/bookmark`, {
+      method: "DELETE",
+      body: JSON.stringify({
+        type,
+        resourceId: id,
+      }),
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${getAccessToken()}`,
+      },
+    });
+  };
+
+  return (
+    <div className={`flex items-center ml-auto ${className}`}>
+      북마크
+      {/* TODO: 특정 ID의 행사 또는 부스가 북마크 상태인지 확인하는 API 구현 후 주석해제하기 */}
+      {/* {isBookmark ? (
+        <FaBookmark onClick={onDeleteBookmark} className="cursor-pointer" />
+      ) : (
+        <FaRegBookmark onClick={onAddBookmark} className="cursor-pointer" />
+      )} */}
+      <FaBookmark onClick={onDeleteBookmark} className="cursor-pointer" />
+      <FaRegBookmark onClick={onAddBookmark} className="cursor-pointer" />
+    </div>
+  );
+}
--- src/Components/MyPage/Bookmark.tsx
+++ src/Components/MyPage/Bookmark.tsx
@@ -1,53 +1,76 @@
+import InfiniteScroll from "react-infinite-scroll-component";
+import { useBookmark } from "../Bookmark/useBookmark";
 import BoothCard from "../Booth/List/BoothCard";
+import EventCard from "../Event/List/EventCard";
+import { useState } from "react";
 
 export default function Bookmark() {
-  const tmpBoothList = [
-    {
-      endDate: "2024-00-00",
-      id: 25,
-      image:
-        "https://openbookbucket.s3.ap-northeast-2.amazonaws.com/%E1%84%80%E1%85%A1%E1%86%AB%E1%84%89%E1%85%B5%E1%86%A8%E1%84%87%E1%85%AE%E1%84%89%E1%85%B3.png",
-      name: "부스1",
-    },
-    {
-      endDate: "2024-00-00",
-      id: 1,
-      image:
-        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTfvISbILWTN9TaMBJPhWKe9rWc6CwtRJZlWw&s",
-      name: "부스1",
-    },
-    {
-      endDate: "2024-00-00",
-      id: 1,
-      image:
-        "https://img1.daumcdn.net/thumb/R1280x0/?fname=http://t1.daumcdn.net/brunch/service/user/8fXh/image/nZ7e2z99yxb9JzoE0AwQNDN1ft4.jpg",
-      name: "부스1",
-    },
-    {
-      endDate: "2024-00-00",
-      id: 1,
-      image:
-        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS__0S6K2gnU_PqcvLpFU8SGYGyb7x2ZtoisQ&s",
-      name: "부스1",
-    },
-    {
-      endDate: "2024-00-00",
-      id: 1,
-      image:
-        "https://img1.daumcdn.net/thumb/R1280x0/?fname=http://t1.daumcdn.net/brunch/service/user/8fXh/image/nZ7e2z99yxb9JzoE0AwQNDN1ft4.jpg",
-      name: "부스1",
-    },
-  ];
+  const [selectedType, setSelectedType] = useState<"EVENT" | "BOOTH">("EVENT");
+  const { data, fetchNextPage, hasNextPage } = useBookmark(selectedType);
+
   return (
-    <section className="w-full grid grid-cols-2 gap-4">
-      {/* {tmpBoothList.map((booth) => (
-        // <BoothCard
-        //   endDate={booth.endDate}
-        //   id={booth.id}
-        //   image={booth.image}
-        //   name={booth.name}
-        // />
-      ))} */}
+    <section>
+      <div className="flex space-x-4 mb-4">
+        <label>
+          <input
+            type="radio"
+            value="행사"
+            checked={selectedType === "EVENT"}
+            onChange={() => setSelectedType("EVENT")}
+            className="mr-2"
+          />
+          행사
+        </label>
+        <label>
+          <input
+            type="radio"
+            value="부스"
+            checked={selectedType === "BOOTH"}
+            onChange={() => setSelectedType("BOOTH")}
+            className="mr-2"
+          />
+          부스
+        </label>
+      </div>
+      <InfiniteScroll
+        dataLength={data?.pages[0].numberOfElements ?? 6}
+        next={fetchNextPage}
+        hasMore={hasNextPage}
+        loader={<h4 className="text-center my-4">로딩 중...</h4>}
+        endMessage={
+          <p className="text-center font-bold my-8">
+            모든 북마크 {selectedType === "EVENT" ? "행사" : "부스"}를
+            불러왔습니다
+          </p>
+        }
+      >
+        <div className="flex flex-col gap-4">
+          {data?.pages.map((items) =>
+            items.content.map((item) =>
+              item.bookmarkType === "BOOTH" ? (
+                <BoothCard
+                  endDate={item.booth.closeDate}
+                  id={item.booth.id}
+                  image={item.booth.mainImageUrl}
+                  name={item.booth.name}
+                  key={item.booth.id}
+                  eventName={item.booth.eventName}
+                  // tags={item.tags} TODO: 부스 tags 브랜치 머지 후 주석 해제
+                />
+              ) : (
+                <EventCard
+                  endDate={item.event.closeDate}
+                  id={item.event.id}
+                  image={item.event.mainImageUrl}
+                  name={item.event.name}
+                  key={item.event.id}
+                  tags={item.event.tags}
+                />
+              )
+            )
+          )}
+        </div>
+      </InfiniteScroll>
     </section>
   );
 }
