05a22a2d5a459396ec907582ba50450807cb4d91
Goongam <dlwjddn341@naver.com>
2024-10-16 22:09:23 +0900

    feat:  부스, 행사 승인 필터링

---
+++ src/Components/Event/Manage/ManageTableFiltering.tsx
@@ -0,0 +1,57 @@
+interface Props {
+  setFilterStatus: (status: ManageFilterStatus) => void;
+}
+
+export type ManageFilterStatus = "waiting" | "approved" | "rejected" | "all";
+
+export default function ManageTableFiltering({ setFilterStatus }: Props) {
+  return (
+    <div className="flex items-center ml-auto gap-5">
+      <label>
+        <input
+          type="radio"
+          name="filtering"
+          value={"all"}
+          defaultChecked
+          onChange={() => {
+            setFilterStatus("all");
+          }}
+        />
+        모두보기
+      </label>
+      <label>
+        <input
+          type="radio"
+          name="filtering"
+          value={"waiting"}
+          onChange={() => {
+            setFilterStatus("waiting");
+          }}
+        />
+        대기 중
+      </label>
+      <label>
+        <input
+          type="radio"
+          name="filtering"
+          value={"rejected"}
+          onChange={() => {
+            setFilterStatus("rejected");
+          }}
+        />
+        반려
+      </label>
+      <label>
+        <input
+          type="radio"
+          name="filtering"
+          value={"approved"}
+          onChange={() => {
+            setFilterStatus("approved");
+          }}
+        />
+        승인
+      </label>
+    </div>
+  );
+}
